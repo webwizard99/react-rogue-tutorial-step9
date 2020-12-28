e1d697df4cc176e31ab4299a34ff7a9f602225a2
Michael Bragg <michael_bragg99@hotmail.com>
2020-12-28 08:17:54 -0700

    Complete step 9 of tutorial.

---
+++ src/NPCs/Player.js
@@ -0,0 +1,22 @@
+import Entity from "../World/Entity";
+
+class Player extends Entity{
+  attributes = {
+    name: 'Player',
+    ascii: '@',
+    health: 10
+  }
+
+  move(dx, dy) {
+    this.x += dx;
+    this.y += dy;
+  }
+
+  copyPlayer() {
+    let newPlayer = new Player();
+    Object.assign(newPlayer, this);
+    return newPlayer;
+  }
+}
+
+export default Player;
